6bbd99e6e4bdcae0715227efa0a9067b85787f45
ShokhrukhMakhmudov <msshohruh@gmail.com>
2024-11-19 11:43:56 +0500

    added log for mongoose models

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectMongoDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log("MongoDB connected");
+    console.log("MongoDB connected", mongoose.modelNames());
   } catch (error) {
     console.log("Could not connect to MongoDB", error);
   }
